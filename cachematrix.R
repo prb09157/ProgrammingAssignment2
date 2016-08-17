3caa1b49f3e98f03a5e3036bd4d047e189eecaec
prb09157 <simon.fleming@uk.tesco.com>
2016-08-17 20:50:16 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Assignment:Caching the Inverse of a Matrix
-## Matrix inversion can be a costly computation, with this there may be some 
-## advantage to caching the inverse of a matrix compared with computing it ##repeatedly.
-## Examples below show a pair of functions that are used to create a unique  
-## object that cache the inverse of a matrix.
+# Assignment:Caching the Inverse of a Matrix
+# Matrix inversion can be a costly computation, with this there may be some 
+# advantage to caching the inverse of a matrix compared with computing it ##repeatedly.
+# Examples below show a pair of functions that are used to create a unique  
+# object that cache the inverse of a matrix.
 
 
 MakeCache <- function(x = matrix()) {
@@ -19,13 +19,13 @@ MakeCache <- function(x = matrix()) {
 }
 
 
-## The below function computes the inverse of the unique "matrix" from the
-## MakeCacheMatrix above. It checks if the inverse has already been calculated,
-## if so it gets the result and skips the computation, otherwise it should 
-## then retrieve the inverse from the cache.
+# The below function computes the inverse of the unique "matrix" from the
+# MakeCacheMatrix above. It checks if the inverse has already been calculated,
+# if so it gets the result and skips the computation, otherwise it should 
+# then retrieve the inverse from the cache.
 
 Solve <- function(x, ...) {
-  ## Return a matrix the inverse of 'x'
+  # Return a matrix the inverse of 'x'
   inv <- x$getInverse()
   if (!is.null(inv)) {
     message("Test R")
@@ -36,3 +36,32 @@ Solve <- function(x, ...) {
   x$setInverse(inv)
   inv
 }
+
+#> source('~/Downloads/ProgrammingAssignment2-master-2/cachematrix.R')
+#> m_1 <- MakeCache(matrix(1:4, 2, 2))
+#> m_1$get()
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> m_1$getInverse()
+#NULL
+#> Solve(m_1)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> m_1$set(matrix(c(4, 4, 2, 1), 2, 2))
+#> m_1$get()
+#[,1] [,2]
+#[1,]    4    2
+#[2,]    4    1
+#> m_1$getInverse()
+#NULL
+#> Solve(m_1)
+#[,1] [,2]
+#[1,] -0.25  0.5
+#[2,]  1.00 -1.0
+#> Solve(m_1)
+#Test R
+#[,1] [,2]
+#[1,] -0.25  0.5
+#[2,]  1.00 -1.0
